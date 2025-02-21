7439084f22a39a07831a0a449f5c5793636b976c
Nariouss <166618558+Nariouss@users.noreply.github.com>
2025-02-21 06:50:37 +0100

    Update bot.py
    Fixing a few errors

--- bot.py
+++ bot.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
     start_flask()
 
 TOKEN = "MTM0MjMyNzY3Njk5NTg5OTQ4Mw.G7gTJC.asmdXKHVSYVrSRlLq7jCA0q_ZXVdVNJZiWBJNA"
-MESSAGE_ID_ROLES = 1342344847583084565
-MESSAGE_ID_FLAGS = 1342369910961078402
+MESSAGE_ID_RULES = 1342344847583084565
+MESSAGE_ID_COLOR = 1342369910961078402
+MESSAGE_ID_FLAGS = 1342372377719210024
 RULE_ROLES = {
     "✅": 1342291211729375292,
 }
@@ -44,7 +45,7 @@ async def on_raw_reaction_add(payload):
     member = guild.get_member(payload.user_id)
     if not member:
         return
-    if payload.message_id == MESSAGE_ID_ROLES and payload.emoji.name in RULE_ROLES:
+    if payload.message_id == MESSAGE_ID_RULES and payload.emoji.name in RULE_ROLES:
         role = guild.get_role(RULE_ROLES[payload.emoji.name])
         if role:
             await member.add_roles(role)
@@ -60,7 +61,7 @@ async def on_raw_reaction_remove(payload):
     member = guild.get_member(payload.user_id)
     if not member:
         return
-    if payload.message_id == MESSAGE_ID_ROLES and payload.emoji.name in RULE_ROLES:
+    if payload.message_id == MESSAGE_ID_RULES and payload.emoji.name in RULE_ROLES:
         role = guild.get_role(RULE_ROLES[payload.emoji.name])
         if role:
             await member.remove_roles(role)
