b92f561249851bf6759004e10a52f3bff4f49f6f
Nariouss <166618558+Nariouss@users.noreply.github.com>
2025-02-23 20:39:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -140,6 +140,6 @@ async def ping(ctx):
     message = await ctx.reply(f'```ini\n[ Pong ]\nTook: ...ms```')
     end_time = datetime.datetime.now(datetime.timezone.utc)
     ms = (end_time - start_time).total_seconds() * 1000
-    await message.edit(content=(f'```ini\n[ Pong ]\nTook: {int(ms)}ms```')
+    await message.edit(content=f'```ini\n[ Pong ]\nTook: {int(ms)}ms```')
 
 bot.run(TOKEN)
